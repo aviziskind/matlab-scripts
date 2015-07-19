54d884370cdc85c7dd0b556a604281907b7d5dd8
Avi Ziskind (XPS M1330) <avi.ziskind@gmail.com>
2015-07-19 02:01:14 -0400

    fixed KNN

--- geometry/kNearestNeighbors_Matlab.m
+++ geometry/kNearestNeighbors_Matlab.m
@@ -1,9 +1,10 @@
 function [idx_K_nbrs, dist_sqr_to_nbrs] = kNearestNeighbors_Matlab(X, K)
     [m,N] = size(X);
-    assert(m < N);
-    idx_K_nbrs = zeros(K,N, 'int16');
+%     assert(m < N);
+    k = min(K, N-1);
+    idx_K_nbrs = zeros(k,N, 'int16');
     if nargout > 1
-        dist_sqr_to_nbrs = zeros(K,N);
+        dist_sqr_to_nbrs = zeros(k,N);
     end
     for i = 1:N
         dists_sqr_i = sum(bsxfun(@minus, X, X(:,i)).^2, 1);
@@ -13,9 +14,9 @@ function [idx_K_nbrs, dist_sqr_to_nbrs] = kNearestNeighbors_Matlab(X, K)
 %         dist_sqr_to_nbrs(:,i) = k_dists(2:K+1);    
 
         [x_sorted, idx] = sort(dists_sqr_i, 'ascend');
-        idx_K_nbrs(:,i) = idx(2:K+1); % idx1 = point i itself (dist = 0)
+        idx_K_nbrs(:,i) = idx(2:k+1); % idx1 = point i itself (dist = 0)
         if nargout > 1
-            dist_sqr_to_nbrs(:,i) = x_sorted(2:K+1);    
+            dist_sqr_to_nbrs(:,i) = x_sorted(2:k+1);    
         end
 
     end     
