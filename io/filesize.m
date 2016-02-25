925b86f3909189ee85b269937cafe4d2ad357d19
Avi Ziskind <avi.ziskind@gmail.com>
2016-02-25 00:33:12 -0500

    filesize changes

--- io/filesize.m
+++ io/filesize.m
@@ -1,4 +1,17 @@
-function sz = filesize(filename)
+function sz = filesize(filename, units)
     s = dir(filename);
     sz = s.bytes;
+    
+    if nargin < 2
+        return;
+    end
+    
+    if strcmpi(units, 'KB')
+        sz = sz / 1024;
+    elseif strcmpi(units, 'MB')
+        sz = sz / 1024^2;
+    elseif strcmpi(units, 'GB')
+        sz = sz / 1024^3;
+    end 
+    
 end
